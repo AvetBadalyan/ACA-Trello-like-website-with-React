665b4d2e2968b66dcd05df5db00de298637ccd13
AvetBadalyan <avetbadalyan@gmail.com>
2022-03-30 22:45:49 +0400

    feat: creating routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import Boards from "./Pages/Boards/Boards";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/Profile";
+import Signin from "./Pages/Sign-in/Signin";
+import SingleTask from "./Pages/SingleTask/SingleTask";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Visit />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Boards" element={<Boards />}>
+            <Route path="/Boards/:Board">
+              element={<Board />}
+              <Route path="/Boards/:Board/:taskId" element={<SingleTask />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
