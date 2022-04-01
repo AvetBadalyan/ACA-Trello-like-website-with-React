848aed9fa9829eec446160da179ad57c94cc8c15
AvetBadalyan <avetbadalyan@gmail.com>
2022-04-02 02:22:30 +0400

    feat: displaying initial boards

--- src/Pages/Boards/BoardsPage.js
+++ src/Pages/Boards/BoardsPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import MainHeader from "../../Components/MainHeader";
 import { ACTION_TYPES, useSmartContext } from "../../state/state";
+import "./BoardsPage.css"
 
 export default function Boards() {
   const { state, dispatch } = useSmartContext();
@@ -10,26 +11,8 @@ export default function Boards() {
       <MainHeader />
       <div className="Boards-Container">
         <input className="new-board-creator" placeholder="Create a new board" />
-        <button
-          className="board-add-button"
-          onClick={dispatch({
-            type: ACTION_TYPES.ADD_BOARD,
-            boardName: "New Board",
-            boardId: Math.random(),
-          })}
-        >
-          Add a new board
-        </button>
-
-        <div className="boardContainer">
-          {state.boards.map((item) => (
-            <Boards
-              key={Math.random()}
-              title={item.boardTitle}
-              boardId={item.boardId}
-            />
-          ))}
-        </div>
+        <button className="board-add-button">Add a new board</button>
+        <div className="board">{state.boards.map ((board) => board.boardTitle)} </div>
       </div>
     </>
   );
