00b92a58e398c591aa43f53c1d9f735f96a489c6
AvetBadalyan <avetbadalyan@gmail.com>
2022-04-02 02:31:37 +0400

    feat: add an onclick function on the button for Boards

--- src/Pages/Boards/BoardsPage.js
+++ src/Pages/Boards/BoardsPage.js
@@ -1,3 +1,4 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React, { useContext } from "react";
 import MainHeader from "../../Components/MainHeader";
 import { ACTION_TYPES, useSmartContext } from "../../state/state";
@@ -11,8 +12,18 @@ export default function Boards() {
       <MainHeader />
       <div className="Boards-Container">
         <input className="new-board-creator" placeholder="Create a new board" />
-        <button className="board-add-button">Add a new board</button>
-        <div className="board">{state.boards.map ((board) => board.boardTitle)} </div>
+        <button
+          className="board-add-button"
+         // onClick={dispatch({
+         //   type: ACTION_TYPES.ADD_BOARD,
+          //  boardId: Math.random(),
+         // })}
+        >
+          Add a new board
+        </button>
+        <div className="board">
+          {state.boards.map((board) => board.boardTitle)}{" "}
+        </div>
       </div>
     </>
   );
